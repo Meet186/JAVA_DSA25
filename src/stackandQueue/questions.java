a0d72a45f8aa18d63d05745b3619a1bebff3d1ae
Meet Solanki <meetsolanki7974@gmail.com>
2025-08-31 14:55:42 +0530

    changes in some files

--- src/stackandQueue/questions.java
+++ src/stackandQueue/questions.java
@@ -1,24 +1,83 @@
 package stackandQueue;
+
 import java.util.*;
+
 public class questions {
 
     // find nextSmaller element.
-    static int[] nextSmallerElement(int[] arr){
+    static int[] nextSmallerElement(int[] arr) {
         int[] ans = new int[arr.length];
         Stack<Integer> st = new Stack<>();
         for (int i = 0; i < arr.length; i++) {
-            while (!st.isEmpty() && st.peek() >= arr[i]){
-                 st.pop();
+            while (!st.isEmpty() && st.peek() >= arr[i]) {
+                st.pop();
             }
             ans[i] = st.isEmpty() ? -1 : st.peek();
             st.push(arr[i]);
         }
         return ans;
     }
+
+    //https://leetcode.com/problems/valid-parentheses/
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (ch == ')') {
+                    if (st.isEmpty() || st.pop() != '(') return false;
+                }
+                if (ch == ']') {
+                    if (st.isEmpty() || st.pop() != '[') return false;
+                }
+                if (ch == '}') {
+                    if (st.isEmpty() || st.pop() != '{') return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public boolean isValid2(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                st.push(')');
+            } else if (ch == '[') {
+                st.push(']');
+            } else if (ch == '{') {
+                st.push(']');
+            } else if (st.isEmpty() || st.peek() != ch) {
+                return false;
+            } else {
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+
+    //https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+    public int minAddToMakeValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == ')'){
+                if(!st.isEmpty() && st.peek() == '('){
+                    st.pop();
+                } else {
+                    st.push(ch);
+                }
+            } else {
+                st.push(ch);
+            }
+        }
+
+        return st.size();
+    }
+
     public static void main(String[] args) {
-        int[] arr = {2,1,5,6,2,3};
-        int[] ans = nextSmallerElement(arr);
-        System.out.println(Arrays.toString(ans));
+        String str = "([)]";
+
 
     }
 }
