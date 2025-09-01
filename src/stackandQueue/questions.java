99cd72b00582783b682e7ad20ded7cac214b3d4e
Meet Solanki <meetsolanki7974@gmail.com>
2025-09-02 00:54:22 +0530

    changes in some files

--- src/stackandQueue/questions.java
+++ src/stackandQueue/questions.java
@@ -60,9 +60,9 @@ public class questions {
     //https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
     public int minAddToMakeValid(String s) {
         Stack<Character> st = new Stack<>();
-        for(char ch : s.toCharArray()){
-            if(ch == ')'){
-                if(!st.isEmpty() && st.peek() == '('){
+        for (char ch : s.toCharArray()) {
+            if (ch == ')') {
+                if (!st.isEmpty() && st.peek() == '(') {
                     st.pop();
                 } else {
                     st.push(ch);
@@ -75,9 +75,95 @@ public class questions {
         return st.size();
     }
 
-    public static void main(String[] args) {
-        String str = "([)]";
+    //https://leetcode.com/problems/check-balanced-string/submissions/1756014508/
+    public static boolean isBalanced(String num) {
+        int odd = 0;
+        int even = 0;
+        for (int i = 1; i <= num.length() - 1; i++) {
+            char ch = num.charAt(i);
+            if (i % 2 == 0) {
+                odd += (int) ch;
+            } else {
+                even += (int) ch;
+            }
+        }
 
+        return odd == even;
+    }
+
+    static boolean isBalancedString(String str) { // ((())) || (()))(
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                st.push(ch);
+            } else {
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+
+    static int isBalancedString2(String str) { // ((())) || (()))(
+        Stack<Character> st = new Stack<>();
+        Stack<Character> st2 = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                st.push(ch);
+            } else { // ')'
+                st2.push(ch);
+            }
+        }
+        return Math.abs(st2.size() - st.size());
+    }
+
+    static int[] removeConsecutiveSubsequence(int[] arr) {
+        // Input - {1,2,2,3,10,10,10,4,4,4,5,7,7};
+        // Output - {1,3,5};
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (st.isEmpty() || arr[i] != st.peek()) {
+                st.push(arr[i]);
+            } else {
+                if (arr[i] == st.peek()) {
+                    if (i == arr.length - 1 || arr[i] != arr[i + 1]) {
+                        st.pop();
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        }
+        int[] ans = new int[st.size()];
+        for (int i = ans.length - 1; i >= 0; i--) {
+            ans[i] = st.peek();
+            st.pop();
+        }
+        return ans;
+    }
+
+    static int[] nextSpan(int[] arr){
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if(!st.isEmpty() && arr[st.peek()] < arr[i]){
+                st.pop();
+            } else {
+                ans[i] = 1;
+                st.push(i);
+            }
+
+            ans[i] = i-st.peek() + 1;
+            st.push(i);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {100,80,60,70,60,75,85};
+        int[] ans = nextSpan(arr);
+        System.out.println(Arrays.toString(ans));
 
     }
 }
