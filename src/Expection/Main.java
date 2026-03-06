786591caa733e3e29c7749d8fe89c6925bb2ff31
win <meetsolanki7974@gmail.com>
2026-03-06 14:46:43 +0530

    new add

--- src/Expection/Main.java
+++ src/Expection/Main.java
@@ -11,6 +11,16 @@ public class Main {
         divide(1,0);
     }
 
+    public static void main (String[] args) throws Exception{
+//        try{
+//            divide(4,0);
+//        } catch (ArithmeticException e ){
+//            System.out.println(e.getMessage());
+//        }
+
+        divide(1,0);
+    }
+
    static int divide (int a , int b) throws Exception{
       if(b == 0){
           throw new ArithmeticException("please do not divide by 0");
